3dbc0d8ce92c1df5f82735307c0ea954f4807427
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-13 16:16:10 -0700

    added dataset

---
+++ src/00-objectball.js
@@ -0,0 +1,25 @@
+function gameObject() {
+    const dataSet = {
+     home: 
+         { teamName: "Brooklyn Nets",
+             colors: ["Black", "White"],
+             players:{ "Alan Anderson":{
+                         number: 0, 
+                         shoe: 16,
+                         points: 22
+                     }
+       }
+     },
+     away: 
+         { teamName:"Charlotte Hornets",
+             colors:["Turquoise", "Purple"],
+             players:{"Jeff Adrien": {
+                 number: 4,
+                 shoe: 18,
+                 points:10
+             }    
+          }
+    } 
+}
+
+}
